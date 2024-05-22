eeca0794fcd2251615c46400733a3025a4e7548c
René <media-r.r@online.de>
2024-05-22 12:00:00 +0200

    03 / Instanzvariablen

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -7,24 +7,32 @@ public class App {
         // Zwei Objekte erzeugen (Befehl "new") (Objekt <-> Instance)
         // System.out.println(new Cat());
         // System.out.println(new Cat());
-//**************------------------------------**************** */
+//**************------------------------------*****************/
 
+        // // Cat --> Refenrenztyp (Klasse aus dem das Objekt entsteht)
+        // // cat1 --> Referenzvariable (Adresse des Objekts im RAM)
+        // Cat cat1 = new Cat();       // Instanziierung >> Objekt
+        // System.out.println(cat1);   // Adresse des Objekts
+
+// System.out.println("-----------------------");
+
+        // Cat cat2 = new Cat();       // Instanziierung >> Objekt
+        // System.out.println(cat2);   // Adresse des Objekts
+    // }
+//**************------------------------------*****************/
+   
         // Cat --> Refenrenztyp (Klasse aus dem das Objekt entsteht)
         // cat1 --> Referenzvariable (Adresse des Objekts im RAM)
         Cat cat1 = new Cat();       // Instanziierung >> Objekt
-        System.out.println(cat1);   // Adresse des Objekts
+        System.out.println("Blick von aussen: " + cat1);   // Text + Adresse des Objekts
+        cat1.sayHi();
+
+System.out.println("-----------------------");
 
         Cat cat2 = new Cat();       // Instanziierung >> Objekt
-        System.out.println(cat2);   // Adresse des Objekts
-
-
-
-
-
-
+        System.out.println("Blick von aussen: " + cat2);   // Tect + Adresse des Objekts
+        cat2.sayHi();
     }
-
-   
-
 }
 
+ 
--- com/cc/java/Cat.java
+++ com/cc/java/Cat.java
@@ -2,4 +2,11 @@ package com.cc.java;
 
 public class Cat {
 
+    // puplic -> öffentlich
+    // void -> no Return (es wird nichts zurück geschickt)
+    public void sayHi(){
+        System.out.println("Blick von innen " + this);   // this --> verweist auf sich selbst
+
+
+    }
 }
