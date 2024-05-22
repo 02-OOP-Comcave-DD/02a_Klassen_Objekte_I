322de4e33b16913ee77542b5645b2626bc50c768
René <media-r.r@online.de>
2024-05-22 10:48:04 +0200

    02 / Referenzvariable zuordnen

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -4,9 +4,20 @@ public class App {
     
     public static void main(String[] args) {
 
-        // Zwei Objekte erzeugen (Befehl "new")
-        System.out.println(new Cat());
-        System.out.println(new Cat());
+        // Zwei Objekte erzeugen (Befehl "new") (Objekt <-> Instance)
+        // System.out.println(new Cat());
+        // System.out.println(new Cat());
+//**************------------------------------**************** */
+
+        // Cat --> Refenrenztyp (Klasse aus dem das Objekt entsteht)
+        // cat1 --> Referenzvariable (Adresse des Objekts im RAM)
+        Cat cat1 = new Cat();       // Instanziierung >> Objekt
+        System.out.println(cat1);   // Adresse des Objekts
+
+        Cat cat2 = new Cat();       // Instanziierung >> Objekt
+        System.out.println(cat2);   // Adresse des Objekts
+
+
 
 
 
