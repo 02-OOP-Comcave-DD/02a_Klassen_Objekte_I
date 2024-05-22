a18d803a6b56288ab56fc04cc566a7edf1cdf5e2
René <media-r.r@online.de>
2024-05-22 10:16:36 +0200

    01 / Objekte durch den Befehl "new" erzeugen

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -4,6 +4,12 @@ public class App {
     
     public static void main(String[] args) {
 
+        // Zwei Objekte erzeugen (Befehl "new")
+        System.out.println(new Cat());
+        System.out.println(new Cat());
+
+
+
 
     }
 
